022915d617bfe6b6c2a7b14fcb9cd6b83520bfcf
Matheswaran K <mathes.k.cse.2021@snsct.org>
2025-09-10 12:41:40 +0530

    Update array.js

--- components/array.js
+++ components/array.js
@@ -1,19 +1,25 @@
 const array = [
     {
-        id: "1",
         Name: "Shivanesh",
         Age: "23",
     },
     {
-        id: "2",
         Name: "Sindhu",
         Age: "22",
     },
     {
-        id: "3",
         Name: "Aakash",
         Age: "23",
     },
+    {
+        Name: "Rakesh",
+        Age: "25",
+    },
+    {
+        Name: "Jenitha",
+        Age: "23"
+    },
 ];
+
 
 export default array;
